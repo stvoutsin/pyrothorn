4dec288a20f429451a83a7e76a1de30b1056b870
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2016-12-16 11:26:54 +0000

    User permissions for temp file in clearwing

--- scripts/setup/apache-clearwing-init.sh
+++ scripts/setup/apache-clearwing-init.sh
@@ -1,7 +1,7 @@
-chmod 775 -R /var/www/html/atlas/static/*
-chmod 775 /var/www/html/atlas/sessions
-chmod 775 -R /var/www/html/atlas/log/*
-chown www-data:994 access.log
-chown www-data:994 error.log
+chmod 755 -R /var/www/html/atlas/static/*
+chmod 755 /var/www/html/atlas/sessions
+chmod 755 -R /var/www/html/atlas/log/*
+chown www-data:994 /var/www/html/atlas/log/
+chown www-data:994 /var/www/html/atlas/static/static_vo_tool/temp/
 
 service apache2 reload
