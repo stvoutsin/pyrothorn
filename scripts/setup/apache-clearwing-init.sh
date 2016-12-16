95b2d3f674707156e186b89aa3f28e625d9beed2
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2016-12-16 12:05:08 +0000

    Changed permissions for sessions folde, clearwing

--- scripts/setup/apache-clearwing-init.sh
+++ scripts/setup/apache-clearwing-init.sh
@@ -1,5 +1,5 @@
 chmod 755 -R /var/www/html/atlas/static/*
-chmod 755 /var/www/html/atlas/sessions
+chmod 775 /var/www/html/atlas/sessions
 chmod 755 -R /var/www/html/atlas/log/*
 chown www-data:994 /var/www/html/atlas/log/
 chown www-data:994 /var/www/html/atlas/static/static_vo_tool/temp/
