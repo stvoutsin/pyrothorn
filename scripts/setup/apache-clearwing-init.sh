27b1496018147fed1d4b908f5bcfe54347442e09
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2017-01-20 14:43:08 +0000

    Small fix to apache-clearwing-init

--- scripts/setup/apache-clearwing-init.sh
+++ scripts/setup/apache-clearwing-init.sh
@@ -1,7 +1,7 @@
 chmod 755 -R /var/www/html/app/static/*
 chmod 775 /var/www/html/app/sessions
 chmod 755 -R /var/www/html/app/log/*
-chown www-data:994 /var/www/html/app/log/
-chown www-data:994 /var/www/html/app/static/static_vo_tool/temp/
+chown -R www-data:994 /var/www/html/app/log/
+chown -R  www-data:994 /var/www/html/app/static/static_vo_tool/temp/
 
 service apache2 reload
