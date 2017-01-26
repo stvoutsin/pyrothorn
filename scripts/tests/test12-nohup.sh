f08336d49f67d7a231ea29e520b6935d38bb6429
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2017-01-26 15:16:00 +0000

    Added a dqp test to test12

--- scripts/tests/test12-nohup.sh
+++ scripts/tests/test12-nohup.sh
@@ -20,7 +20,13 @@
 #
 mkdir logs
 
-# -----------------------------------------------------
+
+
+# -------------------------TEST 1----------------------------
+# Test local TWOMASS and ATLASDR1 from the same JDBC resource.
+# -----------------------------------------------------------
+
+
 # Configure our identity.
 #[root@tester]
 
@@ -40,9 +46,8 @@ mkdir logs
             "${endpointurl:?}/system/info" \
             | bin/pp | tee /tmp/system-info.json
 
-# -----------------------------------------------------
-# Test local TWOMASS and ATLASDR1 from the same JDBC resource.
-#[root@tester]
+
+
 
         source "bin/02-02-create-jdbc-space.sh" \
             'TWOMASS JDBC conection' \
@@ -241,8 +246,12 @@ EOF
 
 
 
-# -----------------------------------------------------
+# -------------------------TEST 2----------------------------
 # Test local TWOMASS and ATLASDR1 from two JDBC resources.
+# -----------------------------------------------------------
+
+
+# 
 #[root@tester]
 
         source "bin/02-02-create-jdbc-space.sh" \
@@ -499,7 +508,9 @@ EOF
 
 
 
-
+# -------------------------TEST 3-----
+# Test ATLAS joined with GAVO.twomass
+# ------------------------------------
 
 
         identity=${identity:-$(date '+%H:%M:%S')}
@@ -572,41 +583,6 @@ EOF
             cat /tmp/gavo-schema.json | self
             )
 
-# --------------------------------------
-# Create the GAIA TAP resource.
-#[root@tester]
-
-        #
-        # Create the IvoaResource
-        source "bin/02-03-create-ivoa-space.sh" \
-            'GAIA TAP service' \
-            'http://gea.esac.esa.int/tap-server/tap'
-        gaiaivoa=${ivoaspace:?}
-
-        #
-        # Import the static VOSI file
-        vosifile='vosi/gaia/gaia-tableset.xml'
-        curl \
-            --header "firethorn.auth.identity:${identity:?}" \
-            --header "firethorn.auth.community:${community:?}" \
-            --form   "vosi.tableset=@${vosifile:?}" \
-            "${endpointurl:?}/${gaiaivoa:?}/vosi/import" \
-            | bin/pp
-
-        #
-        # Find the Gaia DR1 schema
-        findname=gaiadr1
-        curl \
-            --header "firethorn.auth.identity:${identity:?}" \
-            --header "firethorn.auth.community:${community:?}" \
-            --data   "ivoa.resource.schema.name=${findname:?}" \
-            "${endpointurl:?}/${gaiaivoa:?}/schemas/select" \
-            | bin/pp | tee /tmp/gaia-schema.json
-
-        gaiaschema=$(
-            cat /tmp/gaia-schema.json | self
-            )
-
 # -----------------------------------------------------
 # Create a workspace and add the local TWOMASS schema.
 #[root@tester]
@@ -629,19 +605,6 @@ EOF
             "${endpointurl:?}/${queryspace:?}/schemas/import" \
             | bin/pp | tee /tmp/query-schema.json
 
-# -----------------------------------------------------
-# Add the Gaia DR1 schema to our workspace.
-#[root@tester]
-
-        gaianame=gaia
-        curl \
-            --header "firethorn.auth.identity:${identity:?}" \
-            --header "firethorn.auth.community:${community:?}" \
-            --data   "urn:adql.copy.depth=${adqlcopydepth:-THIN}" \
-            --data   "adql.resource.schema.import.name=${gaianame:?}" \
-            --data   "adql.resource.schema.import.base=${gaiaschema:?}" \
-            "${endpointurl:?}/${queryspace:?}/schemas/import" \
-            | bin/pp | tee /tmp/query-schema.json
 
 # -----------------------------------------------------
 # Test queries ...
@@ -667,94 +630,27 @@ EOF
             "${endpointurl:?}/${queryspace:?}/queries/create" \
             | bin/pp | tee /tmp/gavo-query.json
 
-        curl \
-            --silent \
-            --header "firethorn.auth.identity:${identity:?}" \
-            --header "firethorn.auth.community:${community:?}" \
-            --data "adql.query.input=SELECT TOP 2000 designation, ra, dec  FROM gaia.tmass_original_valid WHERE (ra BETWEEN 0 AND 0.5) AND (dec BETWEEN 0 AND 0.5)" \
-            --data "adql.query.status.next=COMPLETED" \
-            --data "adql.query.wait.time=600000" \
-            "${endpointurl:?}/${queryspace:?}/queries/create" \
-            | bin/pp | tee /tmp/gaia-query.json
-
+     
         #
         # Get URLs for the results as VOTable
         wfaudata=$(cat /tmp/wfau-query.json | votable)
         gavodata=$(cat /tmp/gavo-query.json | votable)
-        gaiadata=$(cat /tmp/gaia-query.json | votable)
+
 
         #
         # Get the results as VOTable files
         curl --silent "${wfaudata:?}" | xmlstarlet fo > /tmp/wfau-data.xml
         curl --silent "${gavodata:?}" | xmlstarlet fo > /tmp/gavo-data.xml
-        curl --silent "${gaiadata:?}" | xmlstarlet fo > /tmp/gaia-data.xml
 
         #
         # Remove XML namespaces
         sed -i 's#<VOTABLE[^>]*>#<VOTABLE>#' /tmp/wfau-data.xml
         sed -i 's#<VOTABLE[^>]*>#<VOTABLE>#' /tmp/gavo-data.xml
-        sed -i 's#<VOTABLE[^>]*>#<VOTABLE>#' /tmp/gaia-data.xml
 
-        #
-        # Select a specific row.
-        xmlstarlet sel -t -c "//TR[TD='00012764+0028492']" /tmp/wfau-data.xml | xmlstarlet fo
-        xmlstarlet sel -t -c "//TR[TD='00012764+0028492']" /tmp/gavo-data.xml | xmlstarlet fo
-        xmlstarlet sel -t -c "//TR[TD='00012764+0028492']" /tmp/gaia-data.xml | xmlstarlet fo
 
 # -----------------------------------------------------
 # Create our join query ...
 #[root@tester]
-
-    cat > /tmp/join-query.adql << EOF
-
-    SELECT
-        gaia.tmass_best_neighbour.original_ext_source_id AS gaia_ident,
-        wfau.twomass_psc.designation                     AS wfau_ident,
-
-        gaia.tmass_best_neighbour.source_id              AS best_source_id,
-        gaia.gaia_source.source_id                       AS gaia_source_id,
-
-        wfau.twomass_psc.ra                              AS wfau_ra,
-        gaia.gaia_source.ra                              AS gaia_ra,
-
-        wfau.twomass_psc.dec                             AS wfau_dec,
-        gaia.gaia_source.dec                             AS gaia_dec
-
-    FROM
-        gaia.gaia_source,
-        gaia.tmass_best_neighbour,
-        wfau.twomass_psc
-
-    WHERE
-        gaia.tmass_best_neighbour.source_id = gaia.gaia_source.source_id
-    AND
-        gaia.tmass_best_neighbour.original_ext_source_id = wfau.twomass_psc.designation
-    AND
-        gaia.gaia_source.ra  BETWEEN 0 AND 1.25
-    AND
-        gaia.gaia_source.dec BETWEEN 0 AND 1.25
-    AND
-        wfau.twomass_psc.ra  BETWEEN 0 AND 1.25
-    AND
-        wfau.twomass_psc.dec BETWEEN 0 AND 1.25
-
-EOF
-
-# -----------------------------------------------------
-# Execute our join query.
-#[root@tester]
-
-        curl \
-            --silent \
-            --header "firethorn.auth.identity:${identity:?}" \
-            --header "firethorn.auth.community:${community:?}" \
-            --data-urlencode "adql.query.input@/tmp/join-query.adql" \
-            --data "adql.query.status.next=COMPLETED" \
-            --data "adql.query.wait.time=600000" \
-            "${endpointurl:?}/${queryspace:?}/queries/create" \
-            | bin/pp | tee /tmp/join-query.json
-
-
 
 
     cat > /tmp/join-query.adql << EOF
@@ -803,4 +699,292 @@ EOF
 
 
 
+
+
+
+
+
+
+# -------------------------TEST 4-------------------
+# Test local TWOMASS joined with GAIADR1.gaiasource 
+# --------------------------------------------------
+
+
+        identity=${identity:-$(date '+%H:%M:%S')}
+        community=${community:-$(date '+%A %-d %B %Y')}
+
+        source "bin/01-01-init-rest.sh"
+
+# -----------------------------------------------------
+# Check the system info.
+#[root@tester]
+
+        curl \
+            --silent \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            "${endpointurl:?}/system/info" \
+            | bin/pp | tee /tmp/system-info.json
+
+# -----------------------------------------------------
+# Load the local TWOMASS resource.
+#[root@tester]
+
+        source "bin/02-02-create-jdbc-space.sh" \
+            'TWOMASS JDBC conection' \
+            "jdbc:jtds:sqlserver://${dataname:?}/TWOMASS" \
+            "${datauser:?}" \
+            "${datapass:?}" \
+            "${datadriver:?}" \
+            '*'
+        wfaujdbc=${jdbcspace:?}
+
+        source "bin/03-01-create-adql-space.sh" 'TWOMASS ADQL workspace'
+        wfauadql=${adqlspace:?}
+
+        source "bin/03-04-import-jdbc-metadoc.sh" "${wfaujdbc:?}" "${wfauadql:?}" 'TWOMASS' 'dbo' "meta/TWOMASS_TablesSchema.xml"
+        source "bin/03-04-import-jdbc-metadoc.sh" "${wfaujdbc:?}" "${wfauadql:?}" 'ATLASDR1' 'dbo' "meta/ATLASDR1_TablesSchema.xml"
+
+# --------------------------------------
+# Create the GAIA TAP resource.
+#[root@tester]
+
+        #
+        # Create the IvoaResource
+        source "bin/02-03-create-ivoa-space.sh" \
+            'GAIA TAP service' \
+            'http://gea.esac.esa.int/tap-server/tap'
+        gaiaivoa=${ivoaspace:?}
+
+        #
+        # Import the static VOSI file
+        vosifile='vosi/gaia/gaia-tableset.xml'
+        curl \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --form   "vosi.tableset=@${vosifile:?}" \
+            "${endpointurl:?}/${gaiaivoa:?}/vosi/import" \
+            | bin/pp
+
+        #
+        # Find the Gaia DR1 schema
+        findname=gaiadr1
+        curl \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data   "ivoa.resource.schema.name=${findname:?}" \
+            "${endpointurl:?}/${gaiaivoa:?}/schemas/select" \
+            | bin/pp | tee /tmp/gaia-schema.json
+
+        gaiaschema=$(
+            cat /tmp/gaia-schema.json | self
+            )
+
+# -----------------------------------------------------
+# Create a workspace and add the local TWOMASS schema.
+#[root@tester]
+
+        source "bin/04-01-create-query-space.sh"  'Test workspace'
+        source "bin/04-03-import-query-schema.sh" "${wfauadql:?}" 'TWOMASS' 'wfau'
+        source "bin/04-03-import-query-schema.sh" "${wfauadql:?}" 'ATLASDR1' 'atlas'
+
+# -----------------------------------------------------
+# Add the Gaia DR1 schema to our workspace.
+#[root@tester]
+
+        gaianame=gaia
+        curl \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data   "urn:adql.copy.depth=${adqlcopydepth:-THIN}" \
+            --data   "adql.resource.schema.import.name=${gaianame:?}" \
+            --data   "adql.resource.schema.import.base=${gaiaschema:?}" \
+            "${endpointurl:?}/${queryspace:?}/schemas/import" \
+            | bin/pp | tee /tmp/query-schema.json
+
+# -----------------------------------------------------
+# Test queries ...
+#[root@tester]
+
+        curl \
+            --silent \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data "adql.query.input=SELECT TOP 2000 designation, ra, dec FROM wfau.twomass_psc WHERE (ra BETWEEN 0 AND 0.5) AND (dec BETWEEN 0 AND 0.5)" \
+            --data "adql.query.status.next=COMPLETED" \
+            --data "adql.query.wait.time=600000" \
+            "${endpointurl:?}/${queryspace:?}/queries/create" \
+            | bin/pp | tee /tmp/wfau-query.json
+
+
+        curl \
+            --silent \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data "adql.query.input=SELECT TOP 2000 designation, ra, dec  FROM gaia.tmass_original_valid WHERE (ra BETWEEN 0 AND 0.5) AND (dec BETWEEN 0 AND 0.5)" \
+            --data "adql.query.status.next=COMPLETED" \
+            --data "adql.query.wait.time=600000" \
+            "${endpointurl:?}/${queryspace:?}/queries/create" \
+            | bin/pp | tee /tmp/gaia-query.json
+
+        #
+        # Get URLs for the results as VOTable
+        wfaudata=$(cat /tmp/wfau-query.json | votable)
+        gaiadata=$(cat /tmp/gaia-query.json | votable)
+
+        #
+        # Get the results as VOTable files
+        curl --silent "${wfaudata:?}" | xmlstarlet fo > /tmp/wfau-data.xml
+        curl --silent "${gaiadata:?}" | xmlstarlet fo > /tmp/gaia-data.xml
+
+
+# -----------------------------------------------------
+# Create our join query ...
+#[root@tester]
+
+
+#2212 rows
+    cat > /tmp/join-query.adql << EOF
+
+    SELECT
+        gaia.tmass_best_neighbour.original_ext_source_id AS gaia_ident,
+        wfau.twomass_psc.designation                     AS wfau_ident,
+
+        gaia.tmass_best_neighbour.source_id              AS best_source_id,
+        gaia.gaia_source.source_id                       AS gaia_source_id,
+
+        wfau.twomass_psc.ra                              AS wfau_ra,
+        gaia.gaia_source.ra                              AS gaia_ra,
+
+        wfau.twomass_psc.dec                             AS wfau_dec,
+        gaia.gaia_source.dec                             AS gaia_dec
+
+    FROM
+        gaia.gaia_source,
+        gaia.tmass_best_neighbour,
+        wfau.twomass_psc
+
+    WHERE
+        gaia.tmass_best_neighbour.source_id = gaia.gaia_source.source_id
+    AND
+        gaia.tmass_best_neighbour.original_ext_source_id = wfau.twomass_psc.designation
+    AND
+        gaia.gaia_source.ra  BETWEEN 0 AND 1.25
+    AND
+        gaia.gaia_source.dec BETWEEN 0 AND 1.25
+    AND
+        wfau.twomass_psc.ra  BETWEEN 0 AND 1.25
+    AND
+        wfau.twomass_psc.dec BETWEEN 0 AND 1.25
+
+EOF
+
+#5594
+cat > /tmp/join-query-2.adql << EOF
+
+    SELECT
+        gaia.tmass_best_neighbour.original_ext_source_id AS gaia_ident,
+        wfau.twomass_psc.designation                     AS wfau_ident,
+
+        gaia.tmass_best_neighbour.source_id              AS best_source_id,
+        gaia.gaia_source.source_id                       AS gaia_source_id,
+
+        wfau.twomass_psc.ra                              AS wfau_ra,
+        gaia.gaia_source.ra                              AS gaia_ra,
+
+        wfau.twomass_psc.dec                             AS wfau_dec,
+        gaia.gaia_source.dec                             AS gaia_dec
+
+    FROM
+        gaia.gaia_source,
+        gaia.tmass_best_neighbour,
+        wfau.twomass_psc
+
+    WHERE
+        gaia.tmass_best_neighbour.source_id = gaia.gaia_source.source_id
+    AND
+        gaia.tmass_best_neighbour.original_ext_source_id = wfau.twomass_psc.designation
+    AND
+        gaia.gaia_source.ra  BETWEEN 0 AND 3.25
+    AND
+        gaia.gaia_source.dec BETWEEN 0 AND 1.25
+    AND
+        wfau.twomass_psc.ra  BETWEEN 0 AND 3.25
+    AND
+        wfau.twomass_psc.dec BETWEEN 0 AND 1.25
+
+EOF
+
+
+#10000 (TRUNCATED)
+cat > /tmp/join-query-3.adql << EOF
+
+    SELECT
+        gaia.tmass_best_neighbour.original_ext_source_id AS gaia_ident,
+        wfau.twomass_psc.designation                     AS wfau_ident,
+
+        gaia.tmass_best_neighbour.source_id              AS best_source_id,
+        gaia.gaia_source.source_id                       AS gaia_source_id,
+
+        wfau.twomass_psc.ra                              AS wfau_ra,
+        gaia.gaia_source.ra                              AS gaia_ra,
+
+        wfau.twomass_psc.dec                             AS wfau_dec,
+        gaia.gaia_source.dec                             AS gaia_dec
+
+    FROM
+        gaia.gaia_source,
+        gaia.tmass_best_neighbour,
+        wfau.twomass_psc
+
+    WHERE
+        gaia.tmass_best_neighbour.source_id = gaia.gaia_source.source_id
+    AND
+        gaia.tmass_best_neighbour.original_ext_source_id = wfau.twomass_psc.designation
+    AND
+        gaia.gaia_source.ra  BETWEEN 0 AND 3.25
+    AND
+        gaia.gaia_source.dec BETWEEN 0 AND 3.25
+    AND
+        wfau.twomass_psc.ra  BETWEEN 0 AND 3.25
+    AND
+        wfau.twomass_psc.dec BETWEEN 0 AND 3.25
+
+EOF
+
+# -----------------------------------------------------
+# Execute our join query.
+#[root@tester]
+
+        curl \
+            --silent \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data-urlencode "adql.query.input@/tmp/join-query.adql" \
+            --data "adql.query.status.next=COMPLETED" \
+            --data "adql.query.wait.time=600000" \
+            "${endpointurl:?}/${queryspace:?}/queries/create" \
+            | bin/pp | tee /tmp/join-query.json
+
+
+        curl \
+            --silent \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data-urlencode "adql.query.input@/tmp/join-query-2.adql" \
+            --data "adql.query.status.next=COMPLETED" \
+            --data "adql.query.wait.time=600000" \
+            "${endpointurl:?}/${queryspace:?}/queries/create" \
+            | bin/pp | tee /tmp/join-query.json
+
+
+        curl \
+            --silent \
+            --header "firethorn.auth.identity:${identity:?}" \
+            --header "firethorn.auth.community:${community:?}" \
+            --data-urlencode "adql.query.input@/tmp/join-query-3.adql" \
+            --data "adql.query.status.next=COMPLETED" \
+            --data "adql.query.wait.time=600000" \
+            "${endpointurl:?}/${queryspace:?}/queries/create" \
+            | bin/pp | tee /tmp/join-query.json
 
