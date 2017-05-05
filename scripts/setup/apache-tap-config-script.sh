372126881580daf3069c3b44360c645750da464a
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2017-05-05 15:21:59 +0300

    Fixing ATLASDR3 metadoc

--- scripts/setup/apache-tap-config-script.sh
+++ scripts/setup/apache-tap-config-script.sh
@@ -10,8 +10,8 @@ ProxyPassReverse  ^/firethorn\/adql\/table\/(.*)\/votable$ http://${gillianip:?}
 ProxyPassMatch ^/firethorn\/adql\/query\/(.*)\/votable$    http://${gillianip:?}:8080/firethorn/adql/query/\$1/votable retry=0 connectiontimeout=14400 timeout=14400
 ProxyPassReverse  ^/firethorn\/adql\/query\/(.*)\/votable$ http://${gillianip:?}:8080/firethorn/adql/query/\$1/votable
 
-ProxyPassMatch ^/firethorn\/tap\/atlasdr1\/(.*)$  http://${gillianip:?}:8080/firethorn/tap/${tapserviceid:?}/\$1 retry=0 connectiontimeout=14400 timeout=14400
-ProxyPassReverse  ^/firethorn\/tap\/atlasdr1\/(.*)$  http://${gillianip:?}:8080/firethorn/tap/${tapserviceid:?}/\$1
+ProxyPassMatch ^/firethorn\/tap\/osa\/(.*)$  http://${gillianip:?}:8080/firethorn/tap/${tapserviceid:?}/\$1 retry=0 connectiontimeout=14400 timeout=14400
+ProxyPassReverse  ^/firethorn\/tap\/osa\/(.*)$  http://${gillianip:?}:8080/firethorn/tap/${tapserviceid:?}/\$1
 
 ProxyPassMatch ^/firethorn\/tap\/(.*)$  http://${gillianip:?}:8080/firethorn/tap/\$1  retry=0 connectiontimeout=14400 timeout=14400
 ProxyPassReverse  ^/firethorn\/tap\/(.*)$  http://${gillianip:?}:8080/firethorn/tap/\$1
